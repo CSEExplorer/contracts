cb23b221b8cca104b986d05cd6eb0568c4846b73
AdityaSaxena <saxenaaditya03833@gmail.com>
2026-02-24 22:26:12 +0530

    v1.6.0

--- src/main/java/com/aditya/contracts/event/AggregateTypes.java
+++ src/main/java/com/aditya/contracts/event/AggregateTypes.java
@@ -7,6 +7,6 @@ public final class AggregateTypes {
     public static final String RESTAURANT = "RESTAURANT";
     public static final String MENU_CATEGORY = "MENU_CATEGORY";
     public static final String MENU_ITEM = "MENU_ITEM";
-    public static final String VARIANT = "VARIANT";
+    public static final String VARIANT = "ITEM_VARIANT";
 
 }
